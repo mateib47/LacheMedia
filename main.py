504e86ca45c49ffc7df691a4e96587ebf23d123e
mateib47 <m.bucur@student.utwente.nl>
2022-12-03 21:42:04 +0900

    add query input; cleanup

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 from datetime import date
 from icrawler.builtin import GoogleImageCrawler
 from matplotlib import pyplot as plt
-import os
+import os, shutil
 import cv2
 import numpy as np
 from typing import Optional, Tuple
@@ -39,17 +39,76 @@ def draw_text(img, text,
 
     return text_size
 
+def empty_dir(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+def process_img(titles):
+    for i in range(0, len(os.listdir(base_path))):
+        infile = os.listdir(base_path)[i]
+        print("file : " + infile)
+        name = infile.split('.')[0]
+        print(base_path + infile)
+        img = cv2.imread(base_path + infile + '/000001.jpg')
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        bottomLeftCornerOfText = (10, 500)
+        fontScale = 1
+        fontColor = (255, 255, 255)
+        thickness = 5
+        lineType = 2
+        print(titles)
+
+        height, width, channels = img.shape
+        pos = (10, int(height * 0.70))
+
+        add_text_to_image(
+            img,
+            titles[i],
+            font_color_rgb=(1, 32, 48),
+            top_left_xy=pos,
+            bg_color_rgb=(48, 165, 191),
+            line_spacing=1.5,
+        )
+        image = add_text_to_image(
+            img,
+            'LacheMedia',
+            font_color_rgb=(0, 255, 255),
+            outline_color_rgb=(255, 0, 0),
+            top_left_xy=(10, 10),
+            line_spacing=1.5,
+            font_face=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+        )
+
+        cv2.imshow("img", img)
+        # cv2.imwrite(new_path + str(i) + "out.jpg", img)
+        cv2.waitKey(0)
+
+
 
 print("Starting...")
 
 newsapi = NewsApiClient(api_key=NEWS_KEY)
+empty_dir(base_path)
+empty_dir(new_path)
+print("Query:")
+domain = str(input())
+print("Number of days:")
+days = int(input())
 
 end_day = datetime.datetime.now()
 d = datetime.timedelta(days=30)
 start_day = (end_day - d).strftime('%Y-%m-%d')
 end_day = end_day.strftime('%Y-%m-%d')
 
-all_articles = newsapi.get_everything(q='KO Izzy UFC',
+all_articles = newsapi.get_everything(q=domain,
                                       from_param=start_day,
                                       to=end_day,
                                       language='en',
@@ -58,49 +117,9 @@ titles = []
 for a in all_articles['articles'][0:5]:
     title = a['title']
     titles.append(title)
-    # print(title)
     google_Crawler = GoogleImageCrawler(
         storage={'root_dir': r'images/raw/' + str(len(titles))})
     google_Crawler.crawl(keyword=title, max_num=1)
 
-for i in range(0, len(os.listdir(base_path))):
-    infile = os.listdir(base_path)[i]
-    print("file : " + infile)
-    name = infile.split('.')[0]
-    print(base_path + infile)
-    img = cv2.imread(base_path + infile + '/000001.jpg')
+process_img(titles)
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    bottomLeftCornerOfText = (10, 500)
-    fontScale = 1
-    fontColor = (255, 255, 255)
-    thickness = 5
-    lineType = 2
-    print(titles)
-
-    height, width, channels = img.shape
-    pos = (10, int(height * 0.70))
-    # w, h = draw_text(img, titles[i], pos=pos)
-    # draw_text(img, 'LacheMedia', font_scale=4, pos=(0, 20 + h), text_color_bg=(255, 0, 0))
-
-    add_text_to_image(
-        img,
-        titles[i],
-        font_color_rgb=(1, 32, 48),
-        top_left_xy=pos,
-        bg_color_rgb=(48, 165, 191),
-        line_spacing=1.5,
-    )
-    image = add_text_to_image(
-        img,
-        'LacheMedia',
-        font_color_rgb=(0, 255, 255),
-        outline_color_rgb=(255, 0, 0),
-        top_left_xy=(10, 10),
-        line_spacing=1.5,
-        font_face=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
-    )
-
-    cv2.imshow("img", img)
-    cv2.imwrite(new_path+str(i)+"out.jpg", img)
-    cv2.waitKey(0)
